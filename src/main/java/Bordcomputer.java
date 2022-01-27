ef847bb8fdcec539e77da8540b6b0ff4d33afe92
Jan Ortner <jan.ortner@hotmail.de>
2022-01-27 16:34:48 +0100

    Reworked Boardcomputer

--- src/main/java/Bordcomputer.java
+++ src/main/java/Bordcomputer.java
@@ -1,163 +1,286 @@
-import device.CDDevice;
-
 import java.io.FileReader;
-import java.util.Properties;
-import java.util.Scanner;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Properties;
+import java.util.Scanner;
 
-//uebung 1
- 
 public class Bordcomputer {
+
+	//private static String[] deviceName = new String[3];
+	private static String[] installedDevices=new String[3];
+	//private int deviceNr = 0;
 	
 	
-	private String[]  className = new String[2];
-	private int deviceNr = 0;
-	private int option;
-	private Class<?> cls;
-	private static CDDevice CDobj;
+	//Verwendung von Hilfvariable -> Nachfragen
+	Class<?> c;
+	Object aktiveDevice=null;
 	
-	//*********************************
-	private void readConfig() {
-		 try(FileReader reader =  new FileReader("Geraete")) {
-		        Properties properties = new Properties();
-		        properties.load(reader);
-		       
-		        className[deviceNr++] = properties.getProperty("CD");
-		        //className[deviceNr++] = properties.getProperty("Radio");
-		        className[deviceNr++] = properties.getProperty("USB");
-			    System.out.println(className[0]);
-			    System.out.println(className[1]);
-			    //System.out.println(className[2]);
-		       deviceNr = -1;
-		       }catch (Exception e) {;
-		       e.printStackTrace();
-		       }
-		
-	}
-	//*********************************
-	public void changeDevice() {
-		deviceNr = (deviceNr +1 )% 2;
-		
+	public Bordcomputer() {
+		this.readConfig();
 		try {
-			setDevice();
+			this.setDevices();
 		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}	
-		
+		}
 	}
-	//*********************************
-	private void setDevice() throws ClassNotFoundException{
-		cls= Class.forName(className[deviceNr]);
-		System.out.println("class Name is "+cls.getSimpleName());
-		
+	
+	private void readConfig() {
+		try(FileReader reader =  new FileReader("Geraete.config")) {
+			Properties properties = new Properties();
+			properties.load(reader);
+
+			installedDevices[0] = properties.getProperty("CD");
+			installedDevices[1] = properties.getProperty("Radio");
+			installedDevices[2] = properties.getProperty("USB");
+			System.out.println(installedDevices[0]);
+			System.out.println(installedDevices[1]);
+			System.out.println(installedDevices[2]);
+			//deviceNr = -1;
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	//*********************************
+	
+	@SuppressWarnings("deprecation")
+	private void setDevices() throws ClassNotFoundException {
+		if(installedDevices[0] == null) {
+			System.out.println("Kein Device verf�gbar!");
+
+		} else {
+			this.c=Class.forName(this.installedDevices[0]);
+			try {
+				this.aktiveDevice=this.c.newInstance();
+			} catch (InstantiationException | IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+	
 	public void shutdown() {
+		System.out.println("\n Bordcomputer wird heruntergefahren...");
 		System.exit(0);
 	}
-	//*********************************
-	public void showVolume() throws NoSuchMethodException, SecurityException, ReflectiveOperationException, IllegalArgumentException, InvocationTargetException {
-		Method getVolumCall = cls.getDeclaredMethod("getVolume");
-	    System.out.println("The Sound level is "+getVolumCall.invoke(CDobj));
-	}
-	//*********************************
-	public void showOptions() throws SecurityException, IllegalArgumentException, ReflectiveOperationException {
-		Method getOptionMethodCall = cls.getDeclaredMethod("getOptions");
-		String[] methods =(String[]) getOptionMethodCall.invoke(CDobj);
-		for(int i = 0 ; i< methods.length ; i++) {
-			System.out.println( "Option["+(i+1)+"] : "+methods[i]);
+	
+	public void play() {
+		Method methode;
+		try {
+			if(installedDevices[0] != null) {
+			methode = this.c.getDeclaredMethod("play");
+			System.out.println(methode.invoke(this.aktiveDevice));
+			}
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		//System.out.println(methode.invoke(this.aktiveDevice));		
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void changeDevice() {
+		//Method methode;
+		try {
+			this.installedDevices=changeToNext(this.installedDevices);
+			if(installedDevices[0] != null) {
+				this.c=Class.forName(this.installedDevices[0]);
+				this.aktiveDevice=this.c.newInstance();
+				System.out.println("Auf "+this.installedDevices[0]+" gewechselt.");
+			}
+		} catch (SecurityException | IllegalArgumentException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		
+		}
 	}
-	//*********************************
-	public void interOption(int p) throws ReflectiveOperationException, RuntimeException {
-		Method chooseOptiontMethodCall = cls.getDeclaredMethod("chooseOption",int.class);
-		chooseOptiontMethodCall.invoke(CDobj,8);
+	
+	public void next()  {
+		try {
+			TakeMethodsAndRun("next");
+		} catch (SecurityException | IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	//*********************************
-		public void next() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-			Method nextMethodCall = cls.getDeclaredMethod("next");
-			nextMethodCall.invoke(CDobj);
+	public void prev()  {
+		try {
+			TakeMethodsAndRun("prev");
+		} catch (SecurityException | IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		//***************************************
-		public void prev() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-			Method prevMethodCall = cls.getDeclaredMethod("prev");
-			prevMethodCall.invoke(CDobj);
+	}
+	//============ Hilf-Funktionen ==============================
+	private String[] changeToNext(String[] inputFeld) {
+		String[] outputFeld = new String[inputFeld.length];
+		for(int i=0;i<inputFeld.length;i++) {
+			if(i==0) {outputFeld[inputFeld.length-1]=inputFeld[i];}
+			else{outputFeld[i-1]=inputFeld[i];}
 		}
-		//***************************************
-		public void play() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException { 
-			Method playMethodCall = cls.getDeclaredMethod("play");
-			System.out.println(playMethodCall.invoke(CDobj));
+		return outputFeld;
+	}
+	private void TakeMethodsAndRun(String taste)  {
+		Method methode;
+		try {
+			if (installedDevices[0] != null) {
+			methode = this.c.getDeclaredMethod(taste);
+			methode.invoke(this.aktiveDevice);
+			}
+			//methode=this.c.getDeclaredMethod("play");
+			//System.out.println(methode.invoke(this.aktiveDevice));
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+	//============================================================
+	
+	public void louder(int p) {
+		Method methode;
+		try {
+			methode = this.c.getDeclaredMethod("louder");
+			for(int i = 0; i < p; i++) {
+				methode.invoke(this.aktiveDevice);
+				}
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		//***************************************
-		public void lauder(int s) throws ReflectiveOperationException, SecurityException {
-			
-			Method getVolumCall = cls.getDeclaredMethod("getVolume");
-			Method lauderMethodCall = cls.getDeclaredMethod("lauder");
-			for(int i = 1;i <= s ; i++) {
-				if(((int) getVolumCall.invoke(CDobj)) > 7) {
-					System.out.println( "Too much. The highst Sound Level is 7  ");
-					break;
+	}
+	
+
+	public void quieter(int p) {
+		Method methode;
+		try {
+			methode = this.c.getDeclaredMethod("quieter");
+			for(int i = 0; i < p; i++) {
+				methode.invoke(this.aktiveDevice);
+				}
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void showVolume() {
+		if (installedDevices[0] != null) {
+		for(Method m:this.c.getDeclaredMethods()) {
+			if(m.getName()=="getVolume") {
+				try {
+					System.out.println("Die aktuelle Lautst�rke betr�gt " + m.invoke(this.aktiveDevice));
+				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 					}
-				else lauderMethodCall.invoke(CDobj);
 				}
 			}
-		//***************************************
-        public void quieter(int s) throws ReflectiveOperationException, SecurityException {
-			
-			Method getVolumCall = cls.getDeclaredMethod("getVolume");
-			Method quieterMethodCall = cls.getDeclaredMethod("quieter");
-			for(int i = 1;i <= s ; i++) {
-				if(((int) getVolumCall.invoke(CDobj)) < 1) {
-					System.out.println( "Too little. The lowest Sound Level is 1  ");
-					break;
-					}
-				else quieterMethodCall.invoke(CDobj);
-				}
-			}
-		//***************************************
-	public static void main(String[] args) throws SecurityException, IllegalArgumentException, ReflectiveOperationException {
-		// RD , CD oder USB
-		Bordcomputer ob=new Bordcomputer();
-		CDobj = new CDDevice("Never Gone",3);
-		ob.readConfig();
-		ob.changeDevice();
-		ob.showOptions();
-		ob.lauder(2);
-		ob.showVolume();
-		ob.quieter(1);
-		ob.showVolume();
-		ob.interOption(8);
-		//******************+++
-		
-		Scanner in = new Scanner(System.in);
-		
-		/*do {
-			System.out.println( "\n1-Lauder \n2-Quieter \n3-Show Volume"
-					+ "\n4-Next \n5-Prev\n9-Number Of Tracks \n10-Shutdown");
-			o = in.nextInt();
-			switch(o) {
-			case 1:
-				
-				break;
-			case 2:
-				Method QuieterMethodCall = cls.getDeclaredMethod("quieter", int.class);
-				stars =(String) QuieterMethodCall.invoke(CDobj, 1);
-				System.out.println( stars);
-			break;
-			case 3:
-				showVolume();
-			break;
-			
-			
-			case 10:shutdown();
-			break;
-			}
-
-		}while(o != 10 );*/
-		//**********************
-		
+		}
 	}
+	public void showOptions() {
+		System.out.println("Folgende Optionen sind verf�gbar:");
 
+		Method methode;
+		try {
+			methode = this.c.getDeclaredMethod("getOptions");
+			String[] erg=(String[]) methode.invoke(this.aktiveDevice);
+			for(String s:erg) {	
+				System.out.println(s);
+			}
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void enterOption(int choice) {
+		Method methode;
+		try {
+			methode = this.c.getDeclaredMethod("getOptions");
+			String[] erg=(String[]) methode.invoke(this.aktiveDevice);
+			
+			for(Method m:this.c.getDeclaredMethods()) {
+				if(m.getName()=="chooseOption") {
+					m.invoke(this.aktiveDevice,erg[choice-1]);
+					}
+				}
+			methode=this.c.getDeclaredMethod("play");
+			System.out.println(methode.invoke(this.aktiveDevice));
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main (String[] args) throws ClassNotFoundException {
+		Bordcomputer b = new Bordcomputer();
+
+		Scanner scanner=new Scanner(System.in);
+		if(installedDevices[0] != "") {
+			System.out.println("Bordcomputer ist betriebsbereit. " + installedDevices[0] + " ist ausgew�hlt");
+			
+		} else {
+			System.out.println("Bordcomputer ist betriebsbereit aber es ist kein Ger�t angeschlossen!");
+		}
+		
+		while(true) {
+			System.out.println("\nFolgende Eingaben sind m�glich: \n "
+					+ "0 - Bordcomputer ausschalten \n "
+					+ "1 - Device wird abgespielt \n "
+					+ "2 - Device wird gewechselt \n "
+					+ "3 - N�chster Sender/Titel wird gew�hlt \n "
+					+ "4 - Voheriger Sender/Titel wird gew�hlt \n "
+					+ "5 - Lautst�rke erh�hen \n "
+					+ "6 - Lautst�rke verringern \n "
+					+ "7 - Aktuelle Lautst�rke anzeigen \n "
+					+ "8 - Zeige verf�gbare Musik/Radio-Optionen an \n "
+					+ "9 - Option eingeben \n ");
+			Integer eingabe=Integer.parseInt(scanner.next());
+			switch(eingabe){
+	        case 0:
+	        	b.shutdown();
+	            break;
+	        case 1:
+	        	b.play();
+	            break;
+	        case 2:
+	        	b.changeDevice();
+	        	b.setDevices();
+	            break;
+	        case 3:
+	        	b.next();
+	        	break;
+	        case 4:
+	        	b.prev();
+	        	break;
+	        case 5:
+	        	System.out.println("Um wieviel soll die Lautst�rke erh�ht werden?");
+	        	Integer p = Integer.parseInt(scanner.next());
+	        	b.louder(p);
+	        	break;
+	        case 6:
+	        	System.out.println("Um wieviel soll die Lautst�rke verringert werden?");
+	        	Integer q = Integer.parseInt(scanner.next());
+	        	b.quieter(q);
+	        	break;
+	        case 7:
+	        	b.showVolume();
+	        	break;
+	        case 8:
+	        	b.showOptions();
+	        	break;
+	        case 9:
+	        	System.out.println("welche Musik-/SenderNr. soll abgespielt werden?");
+	        	Integer nr = Integer.parseInt(scanner.next());
+	        	b.enterOption(nr);
+	        	break;
+	        default:
+	            System.out.println("Falsche Eingabe.");
+	            break;
+	        }
+			
+		}
+		
+
+	}
+	
 }
---
+++ src/main/java/device/UsbPlayer.java
@@ -0,0 +1,81 @@
+package device;
+
+import java.util.ArrayList;
+
+public class UsbPlayer implements Device {
+
+	 ArrayList<String>Playlist=new ArrayList<String>();
+	 int Lautstaerke = 0;
+	 String playTrack="";
+
+	public UsbPlayer() {
+		super();
+		Playlist.add("Musik 01");
+		Playlist.add("Musik 02");
+		Playlist.add("Musik 03");
+		playTrack=Playlist.get(0);
+	}
+
+	
+	@Override
+	public void louder() {
+		if(Lautstaerke < 100) {
+			Lautstaerke += 1;
+		} else {
+			Lautstaerke = 100;
+		}		
+	}
+	
+	@Override
+	public void quiter() {
+		if(Lautstaerke > 0) {
+			Lautstaerke -= 1;
+		} else {
+			Lautstaerke = 0;
+		}
+	}
+
+	@Override
+	public int getVolume() {
+		return this.Lautstaerke;
+	}
+
+	@Override
+	public void next() {
+		int correntIdex=Playlist.indexOf(playTrack);
+		int nextIndex=(correntIdex+1)%Playlist.size();
+		playTrack=Playlist.get(nextIndex);
+	}
+
+	@Override
+	public void prev() {
+		int correntIdex=Playlist.indexOf(playTrack);
+		int nextIndex=Playlist.size()-1;
+		if(correntIdex!=0){nextIndex=(correntIdex-1);}
+		playTrack=Playlist.get(nextIndex);
+	}
+
+	@Override
+	public String getInfoText() {
+		return null;
+	}
+
+	@Override
+	public String[] getOptions() {
+		 String[] options = new String[3];
+		 for(int i=0;i<options.length;i++) {
+			 options[i]=Playlist.get(i);
+		 }
+		return options;
+	}
+
+	@Override
+	public void chooseOption(String opt) {	
+		this.playTrack=this.Playlist.get(Playlist.indexOf(opt));	
+	}
+
+	@Override
+	public String play() {
+		return ("USB: "+this.playTrack+" is playing.");
+	}
+}
