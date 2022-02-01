66f7563e834812a85b76c1d9f56c7272be960659
JPANZO <jpanzo@bpe.local>
2022-02-01 20:17:20 +0100

    TestClass created

---
+++ src/test/java/device/CdPlayerTest.java
@@ -0,0 +1,40 @@
+package device;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CdPlayerTest {
+
+    @Test
+    void louder() {
+    }
+
+    @Test
+    void quieter() {
+    }
+
+    @Test
+    void getVolume() {
+    }
+
+    @Test
+    void next() {
+    }
+
+    @Test
+    void prev() {
+    }
+
+    @Test
+    void getInfoText() {
+    }
+
+    @Test
+    void getItemList() {
+    }
+
+    @Test
+    void play() {
+    }
+}
