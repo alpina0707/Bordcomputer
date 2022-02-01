7e777da52acd40c45251386e34f9921fba353027
sahar <sahmaisary@yahoo.com>
2022-02-01 20:09:32 +0100

    Create the UsbPlayerTest class

--- src/test/java/BordcomputerTest.java
+++ src/test/java/BordcomputerTest.java
@@ -1,35 +1,60 @@
-import org.junit.Test;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class BordcomputerTest {
+class BordComputerTest {
 
-    Bordcomputer BCObj = new Bordcomputer();
+    BordComputer bordcomputer= new BordComputer();
 
-    //@ParameterizedTest
-    //@MethodSource("fizzBuzzData")
-   /* @Test
-    void testFile() {
-        BCObj.readConfig();//"Geraete"
-        File file = new File("src/Geraete.txt");
-        assertThat(file).exists();
-       // assertThat(calculatedState).describedAs(testName).isEqualTo(expectedResult);
-    }*/
+    @ParameterizedTest
+    @MethodSource("BordComputerData")
+    void test(String testName,String optionName, boolean expectedResult)throws SecurityException, IllegalArgumentException, ReflectiveOperationException  {
+        //String[] checkOptions = bordcomputer.showOptions();
+        String[] checkOptions = new String[]{"next","prev","getOptions","lauder","play","chooseOption","quieter","getVolume","getInfoText","setTrackNumber","getCDName","setCDName","getTrackNumber"};
 
-    @Test
-    @DisplayName("look for the File")
-    public void Filetest() {
-        assertThat("abc").isEqualTo(123);
+        List<String>newList=new ArrayList<>();
+        for(int i=0;i<checkOptions.length;i++){
+            newList.add(checkOptions[i]);
+        }
+
+        boolean existOption=checkOptionList(newList,optionName);
+        assertThat(existOption).describedAs(testName).isEqualTo(expectedResult);
     }
 
 
+
+    static Stream<Arguments> BordComputerData () {
+        return Stream.of(
+                Arguments.of("return Option[next] : exists", "next", true),
+                Arguments.of("return Option[prev] : exists", "prev",true),
+                Arguments.of("return Option[getOptions] : exists", "getOptions",true),
+                Arguments.of("return Option[lauder] : exists", "lauder",true),
+                Arguments.of("return Option[play] : exists", "play",true),
+                Arguments.of("return Option[chooseOption] : exists", "chooseOption",true),
+                Arguments.of("return Option[quieter] : exists", "quieter",true),
+                Arguments.of("return Option[getVolume] : exists", "getVolume",true),
+                Arguments.of("return Option[getInfoText] : exists", "getInfoText",true),
+                Arguments.of("return Option[setTrackNumber] : exists", "setTrackNumber",true),
+                Arguments.of("return Option[getCDName] : exists", "getCDName",true),
+                Arguments.of("return Option[setCDName] : exists", "setCDName",true),
+                Arguments.of("return Option[getTrackNumber] : exists", "getTrackNumber",true)
+
+        );
+    }
+
+    //Help-Functions
+    public boolean checkOptionList(List<String> optionList,String optionName){
+       if(optionList.contains(optionName)){
+           return true;
+       }
+        return false;
+    }
 
 }
